b477430052308f9363c3f3f9582ab23e50ea7dd6
Rennan Xisto <rennanxistodev@gmail.com>
2022-06-06 13:21:05 -0300

    atualizando

---
+++ Formulario_em_REACT/form/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import MyForm from './components/MyForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>FORMS</h2>
+      <MyForm/>
+    </div>
+  );
+}
+
+export default App;
